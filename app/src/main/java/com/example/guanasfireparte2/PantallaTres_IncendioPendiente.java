ae8f844405b117c4c4d5f0ba4e52285e9a98ea29
KiimberlyZuniga <kiimzuniiga@gmail.com>
2020-06-20 14:52:50 -0600

    Initial commit

---
+++ app/src/main/java/com/example/guanasfireparte2/PantallaTres_IncendioPendiente.java
@@ -0,0 +1,14 @@
+package com.example.guanasfireparte2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PantallaTres_IncendioPendiente extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla_tres__incendio_pendiente);
+    }
+}
